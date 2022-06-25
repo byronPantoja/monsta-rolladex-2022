3de98e781da671a5faf65839f304a1599a4da1a8
byronPantoja <byronpantoja@icloud.com>
2022-06-25 23:14:42 +0800

    learning state, setState, secondary callback
    Optimizing setState in class components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import { Component } from 'react';
+import {Component} from 'react';
 import './App.css';
 import logo from './logo.svg';
 
 class App extends Component {
-  constructor () {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
-      name: 'Bee'
+      name: {firstName: 'Bee', lastName: 'Zee'},
+      company: 'home',
     }
   }
       render() {
+        const {lastName, firstName} = this.state.name;
         return (
           <div className="App">
             <header className="App-header">
               <img src={logo} className="App-logo" alt="logo" />
               <p>
-                hi {this.state.name}
+                Hi I'm {firstName} {lastName}, I work from {this.state.company}.
               </p>
-              <button>
+              <button
+              onClick={()=> {
+                this.setState(() => {
+                  return {
+                    name: {firstName: 'Dev', lastName: 'Bee'},
+                  };
+                }, () => {
+                      console.log(this.state)
+                    }
+                );
+              }}
+              >
                 Change Name
               </button>
             </header>
